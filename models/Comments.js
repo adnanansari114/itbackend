43a5fd5dda4690c8a43a02ef853f9944d1d1b4bd
adnanansari114 <adnanansari.aa.114@gmail.com>
2025-12-10 17:37:22 +0530

    Backend files

--- models/Comments.js
+++ models/Comments.js
@@ -1,7 +1,21 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+
+// const commentSchema = new mongoose.Schema({
+//   blogId: { type: mongoose.Schema.Types.ObjectId, ref: 'Blog' },
+//   blogtitle: String,
+//   name: String,
+//   email: String,
+//   website: String,
+//   comment: String,
+//   createdAt: { type: Date, default: Date.now }
+// });
+
+// module.exports = mongoose.model("Comment", commentSchema);
+
+import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema({
-  blogId: { type: mongoose.Schema.Types.ObjectId, ref: 'Blog' },
+  blogId: { type: mongoose.Schema.Types.ObjectId, ref: "Blog" },
   blogtitle: String,
   name: String,
   email: String,
@@ -10,4 +24,9 @@ const commentSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Comment", commentSchema);
+// Yeh line badlo
+// module.exports = mongoose.model("Comment", commentSchema);
+
+// Nayi line → ESM style
+const Comment = mongoose.model("Comment", commentSchema);
+export default Comment;
