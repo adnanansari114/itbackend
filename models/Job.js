d2d7ed383829ec461f3174638a7d1248c391d126
adnanansari114 <adnanansari.aa.114@gmail.com>
2025-12-10 16:28:48 +0530

    Backend files

--- models/Job.js
+++ models/Job.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const jobSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  techstack: { type: String, required: true },
   company: { type: String, required: true },
+  status: {type: String},
   location: { type: String, required: true },
-  jobType: { type: String, required: true }, 
-  category: { type: String, required: true },
-  salary: String,
+  engagementtype: { type: String, required: true },
+  budget: {type: String},
   description: { type: String, required: true },
   isActive: { type: Boolean, default: true },
   createdAt: { type: Date, default: Date.now }
