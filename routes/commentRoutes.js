90ef3ff76c3bde62079522449fb27002262e7d72
adnanansari114 <adnanansari.aa.114@gmail.com>
2025-12-11 17:25:53 +0530

    Backend files

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -42,11 +42,11 @@ router.delete("/delete/:id", async (req, res) => {
 
 router.post("/add", async (req, res) => {
   try {
-    const { blogId, blogTitle, name, email, comment, parentId = null } = req.body;
+    const { blogId, blogtitle, name, email, comment, parentId = null } = req.body;
 
     const newComment = new Comment({
       blogId,
-      blogTitle,
+      blogtitle,
       name,
       email: email || "", // reply mein email optional
       website,
