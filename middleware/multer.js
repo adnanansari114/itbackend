0b1750f91df51159dc360cd6f4cea7c393bf01d0
adnanansari114 <adnanansari.aa.114@gmail.com>
2025-12-12 11:06:33 +0530

    Backend files

---
+++ middleware/multer.js
@@ -0,0 +1,16 @@
+// middleware/multer.js
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // temporary folder
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + "." + file.originalname.split(".").pop());
+  }
+});
+
+const upload = multer({ storage });
+
+export default upload;
